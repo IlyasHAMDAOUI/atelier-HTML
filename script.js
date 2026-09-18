b1b0fd67e0b284f27e71409aaa3344fe1a0cceac
IlyasHAMDAOUI <hamdaoui.muhammad@gmail.com>
2026-09-18 12:27:49 +0200

    requiem

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
-const button = document.querySelector("headerBottom")
-const nav = document.querySelector("#headerBottomNav")
-console.log(button)
+document.addEventListener("DOMContentLoaded", () => {
+    const burgerBtn = document.querySelector("#burgerBtn");
+    const headerBottom = document.querySelector("#headerBottom");
+    const burgerIcon = burgerBtn.querySelector("i");
 
-button.addEventListener("click", function(){
-console.log("tu as cliqué !")
-nav.classList.toggle("is-open")
-})
+    burgerBtn.addEventListener("click", () => {
+        // Alterne la classe 'is-open' sur la section du menu
+        headerBottom.classList.toggle("is-open");
+
+        // Bascule l'icône entre le burger (bars) et la croix (xmark)
+        if (headerBottom.classList.contains("is-open")) {
+            burgerIcon.classList.remove("fa-bars");
+            burgerIcon.classList.add("fa-xmark");
+        } else {
+            burgerIcon.classList.remove("fa-xmark");
+            burgerIcon.classList.add("fa-bars");
+        }
+    });
+});
